8640867b805ee166b9fd2eab635150fb304ebe31
kbraunlich <kbraunlich@gmail.com>
2018-10-26 10:04:11 +0100

    rm import clarte

--- Python/FunctionalDimensionality/funcdim/crossval.py
+++ Python/FunctionalDimensionality/funcdim/crossval.py
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 from scipy.stats import pearsonr
-import clarte as cl
 
 def make_components(data):
     """Factorize an array of mean beta values over all sessions.
