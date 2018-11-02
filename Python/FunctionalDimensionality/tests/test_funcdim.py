b8bab54a5358c0530367ff41a59a89cb2e768ecc
Olivia Guest <guest.olivia@gmail.com>
2018-11-02 13:25:14 +0000

    more tests

--- Python/FunctionalDimensionality/tests/test_funcdim.py
+++ Python/FunctionalDimensionality/tests/test_funcdim.py
@@ -29,54 +29,53 @@ class TestRealData(unittest.TestCase):  # noqa:D101
         res = np.random.random(data.shape)
         self.assertTrue(pre_proc(data, res).shape == data.shape)
 
-#     def test_full_keys(self):  # noqa:D102
-#         # Create an iterator over the 20 subjects.
-#         all_subjects = (self.data[:, :, :, i] for i in range(20))
-#         option = 'full'
-#
-#         # Check the keys are identical.
-#         self.assertEqual(sorted(functional_dimensionality(
-#             all_subjects, 20, self.mask, option=option).keys()),
-#             sorted(output.dictionary_full.keys()))
-#
-#     def test_full_values(self):  # noqa:D102
-#         # Create an iterator over the 20 subjects.
-#         all_subjects = (self.data[:, :, :, i] for i in range(20))
-#         option = 'full'
-#
-#         # Loop through to compare the contents of both dictionaries:
-#         for (key, value) in functional_dimensionality(
-#                 all_subjects, self.nsubs, self.mask, option=option).items():
-#             if key == 'subject_ID':
-#                 self.assertTrue((value == output.dictionary_full[key]).all())
-#             else:
-#                 self.assertTrue(np.allclose(value,
-#                                             output.dictionary_full[key]))
-#
-#     def test_mean_keys(self):  # noqa:D102
-#         # Create an iterator over the 20 subjects.
-#         all_subjects = (self.data[:, :, :, i] for i in range(20))
-#         option = 'mean'
-#
-#         # Check the keys are identical.
-#         self.assertEqual(sorted(functional_dimensionality(
-#             all_subjects, 20, self.mask, option=option).keys()),
-#             sorted(output.dictionary_mean.keys()))
-#
-#     def test_mean_values(self):  # noqa:D102
-#         # Create an iterator over the 20 subjects.
-#         all_subjects = (self.data[:, :, :, i] for i in range(20))
-#         option = 'mean'
-#
-#         # Loop through to compare the contents of both dictionaries:
-#         for (key, value) in functional_dimensionality(
-#                 all_subjects, self.nsubs, self.mask, option=option).items():
-#             if key == 'subject_ID':
-#                 self.assertTrue((value == output.dictionary_mean[key]).all())
-#             else:
-#                 self.assertTrue(np.allclose(value,
-#                                             output.dictionary_mean[key]))
-#
+    def test_full_keys(self):  # noqa:D102
+        # Create an iterator over the 20 subjects.
+        all_subjects = (self.data[:, :, :, i] for i in range(20))
+        option = 'full'
+
+        # Check the keys are identical.
+        self.assertEqual(sorted(functional_dimensionality(
+            all_subjects, 20, self.mask, option=option).keys()),
+            sorted(output.dictionary_full.keys()))
+
+    def test_full_values(self):  # noqa:D102
+        # Create an iterator over the 20 subjects.
+        all_subjects = (self.data[:, :, :, i] for i in range(20))
+        option = 'full'
+
+        # Loop through to compare the contents of both dictionaries:
+        for (key, value) in functional_dimensionality(
+                all_subjects, self.nsubs, self.mask, option=option).items():
+            if key == 'subject_ID':
+                self.assertTrue((value == output.dictionary_full[key]).all())
+            else:
+                self.assertTrue(np.allclose(value,
+                                            output.dictionary_full[key]))
+
+    def test_mean_keys(self):  # noqa:D102
+        # Create an iterator over the 20 subjects.
+        all_subjects = (self.data[:, :, :, i] for i in range(20))
+        option = 'mean'
+
+        # Check the keys are identical.
+        self.assertEqual(sorted(functional_dimensionality(
+            all_subjects, 20, self.mask, option=option).keys()),
+            sorted(output.dictionary_mean.keys()))
+
+    def test_mean_values(self):  # noqa:D102
+        # Create an iterator over the 20 subjects.
+        all_subjects = (self.data[:, :, :, i] for i in range(20))
+        option = 'mean'
+
+        # Loop through to compare the contents of both dictionaries:
+        for (key, value) in functional_dimensionality(
+                all_subjects, self.nsubs, self.mask, option=option).items():
+            if key == 'subject_ID':
+                self.assertTrue((value == output.dictionary_mean[key]).all())
+            else:
+                self.assertTrue(np.allclose(value,
+                                            output.dictionary_mean[key]))
 
 
 class TestSimData(unittest.TestCase):  # noqa:D101
@@ -87,44 +86,44 @@ class TestSimData(unittest.TestCase):  # noqa:D101
         mask_dim = int(np.round(np.power(self.nvoxels, 1 / 3)))
         self.mask = np.ones((mask_dim, mask_dim, mask_dim), dtype='bool')
         self.data = demo_data(nvoxels=self.nvoxels, nsubs=self.nsubs,
-                         functional_dims=5, nconditions=16)
+                              functional_dims=5, nconditions=16)
 
     def test_covdiag(self):  # noqa:D102
         data = self.data[0][0]
         self.assertTrue(covdiag(data).shape == (self.nsubs, self.nsubs))
 
     def test_pre_proc(self):  # noqa:D102
-        data =self.data[0]
+        data = self.data[0]
         res = np.random.random(data.shape)
         self.assertTrue(pre_proc(data, res).shape == data.shape)
 
-#     def test_full(self):  # noqa:D102
-#         for d in range(1, 10):
-#             # Get some randomly generated data with d dimensions as ground
-#             # truth:
-#             data = demo_data(nvoxels=self.nvoxels, nsubs=self.nsubs,
-#                              functional_dims=d, nconditions=16)
-#             # Create an iterator over the 20 subjects.
-#             all_subjects = (data[:, :, :, i] for i in range(20))
-#             option = 'full'
-#
-#             self.assertEqual(d, functional_dimensionality(
-#                 all_subjects, self.nsubs, self.mask, option=option)
-#                 ['winning_model'].mean())
-#
-#     def test_mean(self):  # noqa:D102
-#         for d in range(1, 10):
-#             # Get some randomly generated data with d dimensions as ground
-#             # truth:
-#             data = demo_data(nvoxels=self.nvoxels, nsubs=self.nsubs,
-#                              functional_dims=d, nconditions=16)
-#             # Create an iterator over the 20 subjects.
-#             all_subjects = (data[:, :, :, i] for i in range(20))
-#             option = 'mean'
-#
-#             self.assertEqual(d, functional_dimensionality(
-#                 all_subjects, self.nsubs, self.mask, option=option)
-#                 ['winning_model'].mean())
+    def test_full(self):  # noqa:D102
+        for d in range(1, 10):
+            # Get some randomly generated data with d dimensions as ground
+            # truth:
+            data = demo_data(nvoxels=self.nvoxels, nsubs=self.nsubs,
+                             functional_dims=d, nconditions=16)
+            # Create an iterator over the 20 subjects.
+            all_subjects = (data[:, :, :, i] for i in range(20))
+            option = 'full'
+
+            self.assertEqual(d, functional_dimensionality(
+                all_subjects, self.nsubs, self.mask, option=option)
+                ['winning_model'].mean())
+
+    def test_mean(self):  # noqa:D102
+        for d in range(1, 10):
+            # Get some randomly generated data with d dimensions as ground
+            # truth:
+            data = demo_data(nvoxels=self.nvoxels, nsubs=self.nsubs,
+                             functional_dims=d, nconditions=16)
+            # Create an iterator over the 20 subjects.
+            all_subjects = (data[:, :, :, i] for i in range(20))
+            option = 'mean'
+
+            self.assertEqual(d, functional_dimensionality(
+                all_subjects, self.nsubs, self.mask, option=option)
+                ['winning_model'].mean())
 
 if __name__ == '__main__':
     unittest.main()
